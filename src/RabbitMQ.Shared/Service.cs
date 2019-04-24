dc86873210912bb8a3b53e58821b5df839ad5fca
Robson <me@rsilva.net>
2019-04-23 22:58:05 -0300

    Working with RPC implementation

--- src/RabbitMQ.Shared/Service.cs
+++ src/RabbitMQ.Shared/Service.cs
@@ -3,9 +3,8 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using RabbitMQ.Shared.Commands;
 using System;
-using System.Diagnostics;
+using System.Collections.Concurrent;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace RabbitMQ.Shared
@@ -15,14 +14,22 @@ namespace RabbitMQ.Shared
         private static Node master;
         private static Node current;
         private static IModel channel;
+        private static BlockingCollection<string> blocking;
 
         private static void Publish<T>(string queue, T content)
         {
             var json = JsonConvert.SerializeObject(content);
             var message = Encoding.UTF8.GetBytes(json);
+            var props = channel.CreateBasicProperties();
 
-            channel.QueueDeclare(queue, false, false, false);
-            channel.BasicPublish(string.Empty, queue, null, message);
+            props.ReplyTo = $"{queue}_reply";
+
+            channel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: queue,
+                basicProperties: null,
+                body: message
+                );
         }
 
         private static void SignToChannels()
@@ -77,21 +84,64 @@ namespace RabbitMQ.Shared
             };
 
             channel = factory.CreateConnection().CreateModel();
+            blocking = new BlockingCollection<string>();
 
-            SignToChannels();
-            AskForMaster(current.Id);
+            //SignToChannels();
+            AskForMaster();
         }
 
-        public static void AskForMaster(string requesterId)
+        public static void AskForMaster()
         {
-            Publish(Queues.ASK_MASTER, new AskForMasterCmd(requesterId));
+            var replyQueueName = channel.QueueDeclare().QueueName;
+            var consumer = new EventingBasicConsumer(channel);
+            var props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
 
-            Task.Delay(5000).Wait();
-
-            if (master == null)
+            consumer.Received += (model, ea) =>
             {
-                current.Master = true;
-            }
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body);
+
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    blocking.Add(response);
+                }
+            };
+
+            channel.BasicPublish(string.Empty, Queues.ASK_MASTER, props, null);
+            channel.BasicConsume(consumer, replyQueueName, true);
+
+            blocking.Take();
+        }
+
+        public static void AssignForAskMaster()
+        {
+            channel.QueueDeclare(Queues.ASK_MASTER);
+            channel.BasicQos(0, 1, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            channel.BasicConsume(Queues.ASK_MASTER, false, consumer);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var props = ea.BasicProperties;
+                var response = string.Empty;
+                var replyProps = channel.CreateBasicProperties();
+
+                replyProps.CorrelationId = props.CorrelationId;
+
+                if (current.Master)
+                {
+                    response = JsonConvert.SerializeObject(current);
+                }
+
+                channel.BasicPublish(string.Empty, props.ReplyTo, replyProps, Encoding.UTF8.GetBytes(response));
+                channel.BasicAck(ea.DeliveryTag, false);
+            };
         }
     }
 }
