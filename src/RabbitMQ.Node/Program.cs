c0d1f22eb3113eff658fdfd1e0f5d6b95d6c5bab
Robson <me@rsilva.net>
2019-06-02 00:34:53 -0300

    Automatic discover of master node

--- src/RabbitMQ.Node/Program.cs
+++ src/RabbitMQ.Node/Program.cs
@@ -11,42 +11,30 @@ namespace RabbitMQ.Node
     {
         public static async Task Main(string[] args)
         {
-            Console.WriteLine("What will it be (C/S)?");
-
-            var cmd = Console.ReadLine();
-
             var factory = new ConnectionFactory()
             {
                 HostName = "localhost",
             };
 
             var channel = factory.CreateConnection().CreateModel();
+            var client = new RPCClient(channel);
+            var cTokenSource = new CancellationTokenSource();
+            cTokenSource.CancelAfter(5000);
 
-            if (cmd == "S")
+            try
+            {
+                Console.WriteLine("Searching for the master...");
+                var message = await client.CallAsync("Hello from the client.", cTokenSource.Token);
+                Console.WriteLine(message);
+            }
+            catch (OperationCanceledException)
             {
                 new RPCServer(channel);
-                Console.WriteLine("This one is a server now.");
+                Console.WriteLine("Master not found. You're the master now.");
             }
-            else if (cmd == "C")
+            catch (Exception ex)
             {
-                var client = new RPCClient(channel);
-                var cTokenSource = new CancellationTokenSource();
-                cTokenSource.CancelAfter(5000);
-
-                try
-                {
-                    Console.WriteLine("This one is a client now.");
-                    var message = await client.CallAsync("Hello from the client!", cTokenSource.Token);
-                    Console.WriteLine(message);
-                }
-                catch (OperationCanceledException ex)
-                {
-                    Console.WriteLine($"No server found: {ex.Message}");
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
+                Console.WriteLine(ex.Message);
             }
 
             Console.Read();
